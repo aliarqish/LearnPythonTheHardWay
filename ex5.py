39b55e1a582dc591488212a8a414f40be07a5df5
Arquish Ali Ansari <aliarqish@gmail.com>
2018-06-16 17:19:26 +0530

    ex5

---
+++ ex5.py
@@ -0,0 +1,20 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s" % my_name
+print "He is %d inches tall" % my_height
+print "He is %d pounds heavy" % my_weight
+print "Actually thats not too heavy"
+print "He's got %s eyes and %s hairs" %(my_eyes,my_hair)
+print "His teeth are usually %r depending on the coffee" % my_teeth
+
+
+print "If I add %d,%d and %d I get %d" %(my_age,my_height,my_weight,my_age+my_height+my_weight)
+print round(1.5)
+print "string"
+print 'string'
